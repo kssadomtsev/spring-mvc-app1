d7d7989a016e7092933f973dc9239bb55f1e5305
kssadomtsev <sadomcev.k@gmail.com>
2021-01-13 21:32:42 +0300

    Lesson 19

--- src/main/java/ru/onyxone/controllers/FirstController.java
+++ src/main/java/ru/onyxone/controllers/FirstController.java
@@ -3,18 +3,23 @@ package ru.onyxone.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println(name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage(){
+    public String goodbyePage() {
         return "first/goodbye";
     }
 }
