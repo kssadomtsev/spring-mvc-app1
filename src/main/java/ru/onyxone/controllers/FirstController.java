fc413adf963d3e9fb82d232e8283354ec55edbbb
kssadomtsev <sadomcev.k@gmail.com>
2021-01-13 22:17:41 +0300

    Lesson 20

--- src/main/java/ru/onyxone/controllers/FirstController.java
+++ src/main/java/ru/onyxone/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.onyxone.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,13 +14,39 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println(name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        //System.out.println(name + " " + surname);
+        model.addAttribute("message", name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodbyePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a,
+                             @RequestParam("b") int b,
+                             @RequestParam("action") String action,
+                             Model model) {
+        double result = 0;
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "division":
+                result = a / (double) b;
+                break;
+        }
+        model.addAttribute("result", result);
+        return "first/calculator";
     }
 }
