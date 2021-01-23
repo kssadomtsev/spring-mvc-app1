5777a9c81db09c303db856b242a42c3542c95cde
Konstantin <sadomcev.k@gmai.com>
2021-01-23 21:01:59 +0300

    lesson 24

--- src/main/java/ru/onyxone/models/Person.java
+++ src/main/java/ru/onyxone/models/Person.java
@@ -1,15 +1,31 @@
 package ru.onyxone.models;
 
+
+import javax.validation.constraints.*;
+
 public class Person {
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 3 and 30")
     private String name;
 
-    public Person(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    @Min(value = 0, message = "Age should greater than 0")
+    private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
 
     public Person() {
+
+    }
+
+    public Person(int id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
     }
 
     public int getId() {
@@ -26,5 +42,21 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
